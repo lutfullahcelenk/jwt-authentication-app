51f678a3839ce2812cc2d6fde7d52448fa8f6c03
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-07-27 23:27:03 +0300

    authApi is setup and configured

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,15 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import { authApi } from "../services/authApi";
+import { setupListeners } from "@reduxjs/toolkit/query/react";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [authApi.reducerPath]: authApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+setupListeners(store.dispatch);
