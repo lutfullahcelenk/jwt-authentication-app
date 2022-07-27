313e4ed4763279fee2f3620751231ff5b896cec2
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-07-27 23:16:09 +0300

    auth form is completed

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -15,13 +15,15 @@ const Auth = () => {
 
   const { firstName, lastName, email, password, confirmPassword } = formValue;
 
-  const handleChange = () => {};
+  const handleChange = (e:any) => {
+    setFormValue({...formValue, [e.target.name]: e.target.value});
+  };
 
   return (
-    <section className="h-screen flex gradient">
-      <div className="m-auto h-1/2 md:h-3/5 bg-slate-800 rounded-xl text-white w-full md:w-2/3 lg:w-3/5 max-w-2xl flex flex-col justify-center items-center">
-        <div className=" mb-10 flex flex-col items-center">
-          <h2 className="uppercase text-xl md:text-3xl tracking-wide">
+    <section className="flex h-screen gradient">
+      <div className="flex flex-col items-center justify-center w-full max-w-2xl m-auto text-white bg-slate-800 rounded-xl md:w-2/3 lg:w-3/5">
+        <div className="flex flex-col items-center mb-10 ">
+          <h2 className="pt-12 text-xl tracking-wide uppercase md:text-3xl">
             {!showRegister ? "Login" : "Register"}
           </h2>
 
@@ -32,48 +34,89 @@ const Auth = () => {
           </p>
         </div>
 
+        {showRegister && (
+          <>
+            <div className="w-full px-6 py-4">
+              <input
+                type="text"
+                className="w-full p-3 text-black rounded shadow-md outline-none shadow-slate-500"
+                value={firstName}
+                onChange={handleChange}
+                placeholder="First Name"
+                name="firstName"
+              />
+            </div>
+
+            <div className="w-full px-6 py-4">
+              <input
+                type="text"
+                className="w-full p-3 text-black rounded shadow-md outline-none shadow-slate-500"
+                value={lastName}
+                onChange={handleChange}
+                placeholder="Last Name"
+                name="lastName"
+              />
+            </div>
+          </>
+        )}
+
         <div className="w-full px-6 py-4">
           <input
             type="email"
-            className="outline-none p-3 w-full rounded shadow-slate-500 shadow-md"
+            className="w-full p-3 text-black rounded shadow-md outline-none shadow-slate-500"
             value={email}
             onChange={handleChange}
             placeholder="Email"
+            name="email"
           />
         </div>
 
         <div className="w-full px-6 py-4">
           <input
             type="password"
-            className="outline-none p-3 w-full rounded shadow-slate-500 shadow-md"
+            className="w-full p-3 text-black rounded shadow-md outline-none shadow-slate-500"
             value={password}
             onChange={handleChange}
             placeholder="Password"
+            name="password"
           />
         </div>
+
+        {showRegister && (
+          <div className="w-full px-6 py-4">
+            <input
+              type="password"
+              className="w-full p-3 text-black rounded shadow-md outline-none shadow-slate-500"
+              value={confirmPassword}
+              onChange={handleChange}
+              placeholder="Confirm Password"
+              name="confirmPassword"
+            />
+          </div>
+        )}
 
         {!showRegister ? (
           <button
             type="button"
-            className="border py-2 px-10 uppercase my-6 rounded-lg"
+            className="px-10 py-2 my-6 uppercase border rounded-lg"
           >
             Login
           </button>
         ) : (
           <button
             type="button"
-            className="border py-2 px-10 uppercase my-6 rounded-lg"
+            className="px-10 py-2 my-6 uppercase border rounded-lg"
           >
             Register
           </button>
         )}
 
-        <div>
+        <div className="pb-12">
           {!showRegister ? (
             <>
               <h5 className="text-lg">Don't have an account?</h5>
               <p
-                className="text-slate-400 text-lg text-center"
+                className="text-lg text-center text-slate-400"
                 onClick={() => setShowRegister(true)}
               >
                 Sign Up
@@ -83,7 +126,7 @@ const Auth = () => {
             <>
               <h5 className="text-lg">Already have an account?</h5>
               <p
-                className="text-slate-400 text-lg text-center"
+                className="text-lg text-center text-slate-400"
                 onClick={() => setShowRegister(false)}
               >
                 Sign In
